db01654f590d11da35f20b8ea6698b7da9f68430
EdwinCavazos <edwinalain911@gmail.com>
2022-08-02 17:19:22 -0500

    Se agrega el archivo con errores de Js con sus respectivas correcciones y algunos comentarios

---
+++ assets/js/funciones.js
@@ -0,0 +1,79 @@
+//Mejores prácticas
+//Simplificar funciones lo mas que se pueda
+
+
+//1. utilizar keyword function
+//2. Nombrar nuestra función    
+//3.Utilizar () y abrir y cerrar la funcion {}
+
+
+function myFunction(){
+    //aqui va el cuerpo de la función
+    console.log("Hola mundo");
+}
+// myFunction();
+
+function myName(){
+    let name= "Edwin";
+    console.log(name)
+    
+}
+// myName();
+function scopeFunction(){
+    let local ="Esto se encuentra dentro de la funcion";
+    console.log(local);
+}
+// scopeFunction();
+// console.log(local);
+var nombre= "Edwin Cavazos";
+function globalScope(){
+    console.log(nombre);
+
+}
+globalScope();
+//Scope de variables
+//Let 
+//Hoisting: la variable no existe para nuestro interprete JS
+    // console.log(x);
+let a="hoisting let";
+//Scope global, local
+
+//Var
+//Hoisting
+//No se puede utilizar antes de declarar aparece como undefined
+// console.log(x);
+var x= "hola";
+//Scope Local, global
+
+//Const
+//Hoisting: la variable no existe para nuestro interprete JS    
+// console.log(c);
+const c = "hoisting const";
+//Scope global, local
+//input
+const miNombre = "Edwin";
+function fiuFiu(x){
+    console.log(x); 
+}
+//parámetro declarado en variable
+fiuFiu(miNombre);
+//parámetro aleatorio 
+fiuFiu("Edwin Cavazos");
+fiuFiu(2);
+
+// fiuFiu(miNombre,2);
+
+
+//funcion con mas de 1 parametro
+function dosParam(x, y){
+    console.log(x+y);
+    // console.log(y);
+}
+//invocacion con dos parámetros
+dosParam("hola", "mundo");
+
+function tresParam(x,y,z){
+    console.log(x+y+z);
+
+}
+tresParam("Hola ","Soy ", "Edwin")
